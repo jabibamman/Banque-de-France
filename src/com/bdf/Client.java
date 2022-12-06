a40eafbc4e65d820c560788407006c9bdb8fb14a
James <james.bidule@gmail.com>
2022-12-06 20:04:41 +0100

    modified

---
+++ src/com/bdf/Client.java
@@ -0,0 +1,83 @@
+package com.bdf;
+
+import java.util.ArrayList;
+
+public class Client extends ArrayList<Client> {
+    private ArrayList<Compte> comptes = new ArrayList<Compte>();
+    private String nom, prenom, lesComptes;
+
+    public Client(){
+        this.comptes = comptes;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.lesComptes = lesComptes;
+
+    }
+
+    public Client(String nom, String prenom) {
+        this.comptes = comptes;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.lesComptes = lesComptes;
+    }
+
+// Getter & Setter
+    public ArrayList<Compte> getComptes() {
+        return comptes;
+    }
+
+    public void setComptes(ArrayList<Compte> comptes) {
+        this.comptes = comptes;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+// Méthode
+
+    public void solde_general(Client nom, Client prenom) {
+        /*
+            for(Compte e : comptes) {
+                if(nom == e.getClient()){
+                    return e;
+                }
+            }
+            return null;
+        }*/
+    }
+
+    public void ajouteCompte(Compte unCompte) {
+        comptes.add(unCompte);
+    }
+
+    public String afficheCompte() {
+        for(Compte c : comptes) {
+            lesComptes += "\n" + c;
+        }
+        return lesComptes;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "comptes=" + getComptes() +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                '}';
+    }
+}
---
+++ src/com/bdf/Main.java
@@ -0,0 +1,47 @@
+package com.bdf;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Banque banque = new Banque();
+
+        Client frederique = new Client("Seger","Frédérique");
+        Client james = new Client("Abib ","James");
+
+        Compte e1 = new Epargne(james,"FR76 12658", 12000, 2);
+        Compte c1 = new Courant(james, "FR76 12658", 6000, false);
+
+        Compte e2 = new Epargne(frederique,"FR76 12558", 8500, 1);
+        Compte c2 = new Courant(frederique, "FR76 12558", 11000, true);
+
+        // la banque ajoute 2 client (frederique seger && james abib)
+        banque.ajouteClient(james);
+        banque.ajouteClient(frederique);
+
+        // ON ajoute les comptes des clients
+        james.ajouteCompte(e1);
+        james.ajouteCompte(c1);
+
+        frederique.ajouteCompte(e2);
+        frederique.ajouteCompte(c2);
+
+        banque.clientLePlusRiche();
+
+//        System.out.println(client.afficheCompte());
+
+        //System.out.println(banque.affifcheClients());
+
+        /*
+        System.out.println(c1.transferer_compte(c2, 3500));
+
+        System.out.println("Compte après virement : \n  Client : " + c1.getClient() + ", Solde : " + c1.getSolde() + "\n  Client : " + c2.getClient() + ", Solde : " + c2.getSolde());
+
+        System.out.println(c1.comparer_compte(c1));
+
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
+*/
+
+    }
+}
+
